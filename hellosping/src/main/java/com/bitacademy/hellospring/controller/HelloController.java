6bc2ee2a52e0bd311a092979673feed6af2ae110
Mose Kim <100126792+MoseKim1214@users.noreply.github.com>
2023-07-17 15:45:10 +0900

    Hello Spring Model, String return

--- hellosping/src/main/java/com/bitacademy/hellospring/controller/HelloController.java
+++ hellosping/src/main/java/com/bitacademy/hellospring/controller/HelloController.java
@@ -1,7 +1,15 @@
 package com.bitacademy.hellospring.controller;
 
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller    //Anotation은 컴파일러가 보고 작동한다.
@@ -29,5 +37,42 @@ public class HelloController {
 		
 		return mav;
 
+	}
+	
+	// 데이터 넘길 때, 이 방법 추천!
+	@RequestMapping("/hello4")
+	public String hello4(String name, Model model) {
+		
+		model.addAttribute("name", name);
+		
+		return "/WEB-INF/views/hello3.jsp";
+
+	}
+	
+	@ResponseBody   // 호출한 메서드에 ResponseBody가 붙어있으면 다르게 해석한다. 리턴하는 애가 message라고 생각.
+	@RequestMapping("/hello5")
+	public String hello5() {
+		
+		return "<h1>Hello World</h1>";
+
+	}
+	
+	
+	// 리다이렉트 방법
+	@RequestMapping("/hello6")
+	public String hello6() {
+		
+		return "redirect:/hello";  //자기가 controller에 맵핑한 부분만 적기
+
+	}
+	
+	
+	// 기술 침투한 예시 (Servlet이 Spring에 침투함)
+	@RequestMapping("/hello7")
+	public void hello7(HttpServletRequest request, HttpServletResponse response, Writer out) throws IOException {
+		
+		String name = request.getParameter("name");
+		out.write("hello" + name);
+		
 	}
 }
